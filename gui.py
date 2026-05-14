449ee24264040a17f96c4357fdb988b8496b1002
haxish3 <haxish3@gmail.com>
2026-05-14 13:36:26 -0300

    centralized gui and error print when downloading posted video (temporary)

--- gui.py
+++ gui.py
@@ -6,16 +6,14 @@ import yt_dlp
 def task_download():
     url = entry.get()
     if not url.startswith("https://"):
-        print("CAIU PORRA")
         return
     opts = {"format": "best", "quiet": True}
 
     try:
         with yt_dlp.YoutubeDL(opts) as dl:
             dl.download([url])
-            print("video baixado!")
     except yt_dlp.utils.DownloadError as e:
-        print("fudido")
+        print(f"Error: {e}")
 
 
 def download():
@@ -27,17 +25,20 @@ app = ck.CTk()
 app.title("PyDload v1.0")
 app.geometry("500x350")
 
-title_label = ck.CTkLabel(app, text="PyDload", font=("Roboto", 24, "bold"))
+main = ck.CTkFrame(app, fg_color="transparent")
+main.pack(expand=True)
+
+title_label = ck.CTkLabel(main, text="PyDload", font=("Roboto", 24, "bold"))
 title_label.pack(padx=10, pady=(30, 20))
 
-d_label = ck.CTkLabel(app, text="Paste Media URL:", font=("Roboto", 12))
+d_label = ck.CTkLabel(main, text="Paste Media URL:", font=("Roboto", 12))
 d_label.pack(padx=10, pady=(10, 0))
 
-entry = ck.CTkEntry(app, width=350, placeholder_text="https://...")
+entry = ck.CTkEntry(main, width=350, placeholder_text="https://...")
 entry.pack(padx=30, pady=10)
 
 d_btn = ck.CTkButton(
-    master=app,
+    master=main,
     text="Fetch Content",
     command=download,
     width=200,
