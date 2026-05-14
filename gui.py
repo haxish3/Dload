754dea1bc450a81ebb27a71e7a897baf04f4c397
haxish3 <haxish3@gmail.com>
2026-05-14 15:32:47 -0300

    project 0

--- gui.py
+++ gui.py
@@ -4,20 +4,73 @@ import yt_dlp
 
 
 def task_download():
+    app.after(
+        0,
+        d_btn.configure,
+        text="Downloading...",
+        text_color="#1A1A1A",
+        fg_color="#475569",
+        hover_color="#475569",
+        state="disabled",
+    )
+
     url = entry.get()
-    if not url.startswith("https://"):
+    if not url.startswith(("http://", "https://")):
+        app.after(
+            0,
+            d_btn.configure,
+            text="Invalid URL",
+            text_color="#ffffff",
+            fg_color="#EF4444",
+            hover_color="#EF4444",
+            state="normal",
+        )
+        norm_btn()
+
         return
+
     opts = {"format": "best", "quiet": True}
 
     try:
         with yt_dlp.YoutubeDL(opts) as dl:
             dl.download([url])
+            app.after(
+                0,
+                d_btn.configure,
+                text="Finished!",
+                text_color="#ffffff",
+                fg_color="#10B981",
+                hover_color="#10B981",
+                state="normal",
+            )
+            norm_btn()
     except yt_dlp.utils.DownloadError as e:
-        print(f"Error: {e}")
+        app.after(
+            0,
+            d_btn.configure,
+            text="Try Again",
+            text_color="#ffffff",
+            fg_color="#EF4444",
+            hover_color="#EF4444",
+            state="normal",
+        )
+        norm_btn()
 
 
 def download():
     Thread(target=task_download).start()
+
+
+def norm_btn():
+    app.after(
+        2000,
+        d_btn.configure,
+        text="Fetch Content",
+        text_color="#FFFFFF",
+        fg_color="#2E75E7",
+        hover_color="#1A58DD",
+        state="normal",
+    )
 
 
 app = ck.CTk()
@@ -35,16 +88,17 @@ d_label = ck.CTkLabel(main, text="Paste Media URL:", font=("Roboto", 12))
 d_label.pack(padx=10, pady=(10, 0))
 
 entry = ck.CTkEntry(main, width=350, placeholder_text="https://...")
-entry.pack(padx=30, pady=10)
+entry.pack(padx=30, pady=(10, 0))
 
 d_btn = ck.CTkButton(
-    master=main,
+    main,
     text="Fetch Content",
     command=download,
     width=200,
     height=40,
-    fg_color="green",
-    hover_color="darkgreen",
+    fg_color="#2E75E7",
+    hover_color="#1A58DD",
+    text_color="#FFFFFF",
     cursor="hand2",
 )
 d_btn.pack(padx=10, pady=20)
